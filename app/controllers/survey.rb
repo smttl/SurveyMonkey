d9d95047f86d79d277d80a6d773fc0c2fc13e52d
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-06 16:31:18 -0500

    add routes to survey.rb

---
+++ app/controllers/survey.rb
@@ -0,0 +1,18 @@
+get '/surveys/new' do
+  erb :'survey/new'
+end
+
+post '/surveys' do
+  @survey = Survey.create(params[:survey])
+  redirect 'users/<%= @user.id %>'
+end
+
+get '/surveys/:id' do
+ @survey = Survey.find(id)
+ erb :'survey/show'
+end
+
+delete '/surveys/:id' do
+  Survey.find(id).destroy
+  redirect 'users/<%= @user.id %>'
+end
